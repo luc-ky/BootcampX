6930d8e95c31c71cc263204459e22f2a1922b171
David Luc <luc.ky@me.com>
2023-05-04 02:24:35 -0400

    Add postgres and initial queries

---
+++ bootcamp_app/students.js
@@ -0,0 +1,58 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "labber",
+  password: "labber",
+  host: "localhost",
+  database: "bootcampx",
+});
+
+/*
+pool
+  .query(`
+SELECT id, name, cohort_id
+FROM students
+LIMIT 5;
+`)
+  .then((res) => {
+    console.log(res.rows);
+  })
+  .catch((err) => console.error("query error", err.stack));
+
+pool
+  .query(`
+SELECT students.id AS student_id,
+  students.name AS name,
+  cohorts.name AS cohort
+FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+LIMIT 5;
+`)
+  .then((res) => {
+    res.rows.forEach((user) => {
+      console.log(
+        `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
+      );
+    });
+  })
+  .catch((err) => console.error("query error", err.stack));
+*/
+
+pool
+  .query(`
+SELECT students.id AS student_id,
+  students.name AS name,
+  cohorts.name AS cohort
+FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+LIMIT ${process.argv[3]};
+`)
+  .then((res) => {
+    res.rows.forEach((user) => {
+      console.log(
+        `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
+      );
+    });
+  })
+  .catch((err) => console.error("query error", err.stack));
